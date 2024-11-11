f7e835a8088550b723bad29a54fc168f85f96e1f
Leslie <lesliesantana2209@yahoo.com>
2024-11-10 21:32:22 -0500

    modal js addition

--- index.js
+++ index.js
@@ -3,22 +3,40 @@
 // VMDuKIoekIuKGcEZV
 function contact(event) {
   event.preventDefault();
-  // emailjs
-  // .sendForm(
-  //     'leslieamanda0504',
-  //     'template_m4vz0j2',
-  //     event.target,
-  //     'VMDuKIoekIuKGcEZV'
-  // )
-  // .then(() => {
-  //     console.log('thisworked1')
-  // }
-  // )
-
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
-  loading.classList.add("modal__overlay--visible")
+  loading.classList.add("modal__overlay--visible");
+  emailjs
+    .sendForm(
+      "leslieamanda0504",
+      "template_m4vz0j2",
+      event.target,
+      "VMDuKIoekIuKGcEZV"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList.add("modal__overlay--visible");
+      console.log("thisworked1");
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible');
+        alert("The email service is temporarily unavailable. Please contact me directly at leslieamanda0504@gmail.com");
+
+    })
+  
   setTimeout(() => {
-    console.log('it worked 1')
-  }, 500);
+    console.log("it worked 1");
+  }, 1000);
 }
+let isModalOpen=false;
+function toggleModal(){
+    
+    if (isModalOpen){
+        isModalOpen= false;
+        return document.body.classList.remove('modal--open');
+    
+    }
+    document.body.classList.add("modal--open");
+    isModalOpen= true
+
+}
+
