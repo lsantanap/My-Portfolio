ea7ccbf7d395cdc2ae5aec9950c6636f60438282
Leslie <lesliesantana2209@yahoo.com>
2024-11-25 07:57:41 -0500

    added boolInt

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-// template_m4vz0j2
-// leslieamanda0504
-// VMDuKIoekIuKGcEZV
 let isModalOpen=false;
 let contrastToggle=false;
+const scaleFactor = 1/20
 
 function moveBackground(event){
   const shapes = document.querySelectorAll(".shape");
-  const x = event.clientX;
-  const y = event.clientY;
-  console.log(x, y)
+  const x = event.clientX * scaleFactor;
+  // Client x and y move the element in this plane (shapes) up, down, left, and right
+  const y = event.clientY * scaleFactor;
+  
+  for (let i = 0; i < shapes.length; ++i){
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1: 1 
+    // Ternary operator, this is a different way of writting an if statement. This line basically means: if isOdd is true, the value is equal to -1; otherwise, set it to 1 
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt }px)`
+    // Since we're changing the value of isOdd by -1 or 1, depending if is true or not, multiplying by this function changes the direction of the movement
+  }
 }
 
 
